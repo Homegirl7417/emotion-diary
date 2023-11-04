fbd62873df97ab41ffe3e73c2f49a1556bcf0ca9
ParkHyunKyeong <kj3477417@gmail.com>
2023-11-04 23:16:29 +0900

    feature: MyHeader 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
+import MyHeader from './components/MyHeader';
+import MyButton from './components/MyButton';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText="App"
+          leftChild={<MyButton text="왼쪽 버튼" onClick={() => alert('왼쪽 클릭')} />}
+          rightChild={<MyButton text="오른쪽 버튼" onClick={() => alert('오른쪽 클릭')} />}
+        />
         <h2>App.js</h2>
         <Routes>
           <Route path="/" element={<Home />} />
