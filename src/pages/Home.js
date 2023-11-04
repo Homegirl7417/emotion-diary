35b0b310a4d890332956cfae09cb5cf8d0e4b0e4
ParkHyunKyeong <kj3477417@gmail.com>
2023-11-04 22:59:14 +0900

    feature: 버튼 컴포넌트 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
+import MyButton from '../components/MyButton';
+
 const Home = () => {
   return (
     <div>
+      <MyButton text="버튼" type="positive" onCLick={() => alert('버튼 클릭!')} />
+      <MyButton text="버튼" type="negative" onCLick={() => alert('버튼 클릭!')} />
+      <MyButton text="버튼" type="default" onCLick={() => alert('버튼 클릭!')} />
+      <MyButton text="버튼" type="asdfsdfsdf" onCLick={() => alert('버튼 클릭!')} />
       <h1>Home</h1>
       <p>이곳은 홈 입니다.</p>
     </div>
